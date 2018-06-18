c2b72060403b0708855e4be20a681eceb9509f98
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-18 15:40:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,9 @@ def oxford_comma(array)
     array[0]
   elsif array.size == 2 
     array.join(" and ")
-  elseif array.size > 2 
+  elsif array.size > 2 
     array_minus_last = array.delete_at(-1)
     output = array_minus_last.join(", ")
     output << ", and #{array[-1]}"
+  end  
 end
